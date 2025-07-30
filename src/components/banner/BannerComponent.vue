b1407bd95866f5dabe57a290d481e582c8eebbc6
nesmanpro <nesmanpro@gmail.com>
2025-07-30 10:16:33 +0200

    feat. table: styling media queries

--- src/components/banner/BannerComponent.vue
+++ src/components/banner/BannerComponent.vue
@@ -12,7 +12,7 @@ const BannerStore = useBannerStore();
 <template>
     <div 
     @click="slideoutStore.openModal"
-    class="group fixed top-4 left-1/2 -translate-x-1/2 rounded-lg text-white bg-emerald-600 shadow-md py-1 px-3 z-50 cursor-pointer h-9 w-45 overflow-hidden">
+    class="group fixed top-15 sm:top-4 left-1/2 -translate-x-1/2 rounded-lg text-white bg-emerald-600 shadow-md py-1 px-3 z-50 cursor-pointer h-9 w-45 overflow-hidden">
         <span class="absolute left-1/2 w-fit text-nowrap -translate-x-1/2 group-hover:-translate-y-[200%] transition-translate duration-90 ease-[cubic-bezier(0.95,0.05,0.795,0.035)] ">Show Sideout Menu</span>
         <span class="absolute left-1/2 w-fit text-nowrap -translate-x-1/2 translate-y-[200%] group-hover:-translate-y-[0%] transition-translate duration-90 ease-[cubic-bezier(0.95,0.05,0.795,0.035)] ">👉 👉 👉</span>
     </div>
